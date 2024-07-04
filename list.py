3f46bb68fb8d7009666e807025b8717bfefcca10
Sukhvinder Kaur <sukhvinderkaur2416@gmail.com>
2024-07-04 10:17:06 +0530

    list operations

--- list.py
+++ list.py
@@ -1,2 +1,36 @@
-mylist = ["apple", "banana", "cherry"]
+mylist = ["apple", "banana", "cherry", "grapes", "papaya"]
+
 print(len(mylist))
+
+print(mylist[2:4])
+
+if "apple" in mylist:
+  print("Yes, 'apple' is in the fruits list")
+  
+mylist[1] = "berry"
+print(mylist)
+
+mylist[1:3] = ["blackcurrant", "watermelon"]
+print(mylist)
+
+mylist.append("orange")
+print(mylist)
+
+mylist.insert(1, "orange")
+print(mylist)
+
+tropical = ["mango", "pineapple", "papaya"]
+mylist.extend(tropical)
+print(mylist)
+
+mylist.remove("blackcurrant")
+print(mylist)
+
+mylist.pop()
+print(mylist)
+
+del mylist[4]
+print(mylist)
+
+mylist.clear()
+print(mylist)
