2167da435db7e8e234037387885977fe4beb29c9
Sukhvinder Kaur <sukhvinderkaur2416@gmail.com>
2024-07-11 10:41:14 +0530

    data cleaning

--- pandaslib.py
+++ pandaslib.py
@@ -1,7 +1,24 @@
 import pandas as pd
+import numpy as np
 
 df = pd.read_csv('data.csv')
+print(df)
 
-df.loc[7,'Duration'] = 45
+df = df.drop([2, 9])
 
-print(df.to_string())
+age = [20, 19, 20, 19, 26, 22, 20, 21]
+
+mean_age = np.mean(age)
+print(mean_age)
+
+age = [mean_age if pd.isna(x) else x for x in age]
+
+df.to_csv('updated_data.csv', index=False)
+
+print("Mean Age:", mean_age)
+print("\nUpdated DataFrame:")
+print(df)
+
+
+
+
