8f79514e943c953d089183c93e4be7da56b5fe4a
Sukhvinder Kaur <sukhvinderkaur2416@gmail.com>
2024-07-11 09:54:14 +0530

    joining arrays

--- numpylib.py
+++ numpylib.py
@@ -6,6 +6,9 @@ arr = np.array([1, 2, 3, 4, 5, 6, 7])
 arr2 = np.array([['a', 'b', 'c', 'd', 'e'], ['f', 'g', 'h', 'i', 'j']])
 print(arr)
 
+for x in arr:
+  print(x)
+
 print(type(arr))
 print(type(arr2))
 
@@ -36,3 +39,12 @@ print(arr3.shape)
 arr4 = np.array([1, 2, 3, 4], ndmin=3)
 print(arr4)
 print('shape of array :', arr4.shape)
+
+arr5 = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+newarr = arr5.reshape(4, 3)
+print(newarr)
+
+arr6 = np.array([1, 2, 3])
+arr7 = np.array([4, 5, 6])
+arr8 = np.concatenate((arr6, arr7))
+print(arr8)
