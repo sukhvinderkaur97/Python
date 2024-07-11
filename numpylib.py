00e0068189d949ffeb832d9d04992cca041e6f30
Sukhvinder Kaur <sukhvinderkaur2416@gmail.com>
2024-07-11 09:30:47 +0530

    operations

--- numpylib.py
+++ numpylib.py
@@ -1,4 +1,11 @@
-import numpy
+import numpy as np
 
-arr = numpy.array([1, 2, 3, 4, 5])
+print(np.__version__)
+
+arr = np.array([1, 2, 3, 4, 5])
+arr2 = np.array([['a', 'b', 'c', 'd', 'e'], ['f', 'g', 'h', 'i', 'j']])
 print(arr)
+print(type(arr))
+print(arr.ndim)
+print(arr2.ndim)
+
