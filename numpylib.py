ec67c9268363edfdbf906bda5c35b8b456314a0d
Sukhvinder Kaur <sukhvinderkaur2416@gmail.com>
2024-07-11 09:40:22 +0530

    arrays operations

--- numpylib.py
+++ numpylib.py
@@ -2,10 +2,16 @@ import numpy as np
 
 print(np.__version__)
 
-arr = np.array([1, 2, 3, 4, 5])
+arr = np.array([1, 2, 3, 4, 5, 6, 7])
 arr2 = np.array([['a', 'b', 'c', 'd', 'e'], ['f', 'g', 'h', 'i', 'j']])
 print(arr)
+
 print(type(arr))
+print(type(arr2))
+
 print(arr.ndim)
 print(arr2.ndim)
+
+print(arr[1:5])
+print(arr[2:7:2])
 
