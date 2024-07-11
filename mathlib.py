726e5cd018d82010d11355af6e931744b04530af
Sukhvinder Kaur <sukhvinderkaur2416@gmail.com>
2024-07-11 10:08:41 +0530

    math module

---
+++ mathlib.py
@@ -0,0 +1,34 @@
+import math
+
+print(math.e)
+print(math.pi)
+print(math.tau)
+
+num = 25
+
+angle = math.radians(30)
+
+sqroot = math.sqrt(num)
+print(sqroot)
+
+fact = math.factorial(num // 5)
+print(fact)
+
+exponen= math.exp(2)
+print(exponen)
+
+logarithm = math.log(num)
+print(logarithm)
+
+sinresult = math.sin(angle)
+print(sinresult)
+
+cosresult = math.cos(angle)
+print(cosresult)
+
+tanresult = math.tan(angle)
+print(tanresult)
+
+degree = math.degrees(angle)
+print(degree)
+
